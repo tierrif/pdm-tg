f0c9d96d2dcaa7069af39c110f8a743080d1e4e0
Francisco Fernandes <72525612+TheBestPT@users.noreply.github.com>
2024-01-10 10:41:16 +0000

    Making main activity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures.viewBinding = true
 }
 
 dependencies {
@@ -43,6 +45,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.room:room-common:2.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/pdm_tg/MainActivity.kt
+++ app/src/main/java/com/example/pdm_tg/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.pdm_tg
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var context : Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.context = applicationContext
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.navigator) as NavHostFragment
+        val navController = navHost.findNavController()
+        val appBarConfig = AppBarConfiguration(navController.graph)
+
+        val toolbar = findViewById<Toolbar>(R.id.appBar)
+
+        setSupportActionBar(toolbar)
+        toolbar.setupWithNavController(navController, appBarConfig)
     }
 }
